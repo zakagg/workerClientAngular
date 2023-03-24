88f1a0f8415b8812d6f576e1d4c9f3f91fd1c75b
Zakaria Aggoun <zakagg@gmail.com>
2023-03-24 11:41:51 +0100

    add unsubscribe

--- src/app/works/work-list.component.ts
+++ src/app/works/work-list.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Subscription } from "rxjs";
 import { IWork } from "./work";
 import { WorkService } from "./works.service";
 
 @Component({selector:"work-list",
             templateUrl:"work-list.component.html",
             styleUrls:["work-list.component.css"]})
-export class WorkListComponent implements OnInit{
+export class WorkListComponent implements OnInit,OnDestroy{
     constructor(private workService:WorkService){}
 
 private _workFilter:string=""
@@ -15,6 +16,7 @@ listeFilterWork:IWork[]=this.listWorks;
 showImage=false;
 imageWidth:number=25;
 imageMargin:number=5;
+sub!:Subscription;
 
 
 get workFilter(){
@@ -27,14 +29,16 @@ set workFilter(value:string){
     this.listeFilterWork=this.searchedWorkFromString(value)
 }
 ngOnInit(){
-    this.workService.getWoreker().subscribe({
-        next:works=>{
-            
+    this.sub=this.workService.getWoreker().subscribe({
+        next:works=>{            
             this.listWorks=works;
             this.listeFilterWork=works;
             },
         error:err=>this.errorMessage=err
     });
+}
+ngOnDestroy(): void {
+    this.sub.unsubscribe();
 }
 toggleImage(){
     this.showImage=!this.showImage
@@ -42,9 +46,6 @@ toggleImage(){
 searchedWorkFromString(value:string){ 
     value=value.toLowerCase(); 
    return this.listWorks.filter((works:IWork)=>
-   works.name.toLowerCase().includes(value))
-   
-   
-
+   works.name.toLowerCase().includes(value))  
 }
 }
